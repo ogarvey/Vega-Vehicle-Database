d3ef73a2b0de085c8aeab28479ae1066775928d7
Oliver Garvey <ogarvey@gmail.com>
2017-08-30 21:00:49 +0100

    Added client side filtering of vehicle list

--- ClientApp/app/components/vehicle-list/vehicle-list.component.ts
+++ ClientApp/app/components/vehicle-list/vehicle-list.component.ts
@@ -1,6 +1,8 @@
+import { KeyValuePair } from '../../models/keyValuePair';
 import { VehicleService } from '../../services/vehicle.service';
 import { Vehicle } from '../../models/vehicle';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs/Observable";
 
 @Component({
     selector: 'vehicle-list',
@@ -9,11 +11,45 @@ import { Component, OnInit } from '@angular/core';
 
 export class VehicleListComponent implements OnInit {
     vehicles: Vehicle[];
-    
+    allVehicles: Vehicle[];
+    makes: any[];
+    models: KeyValuePair[];
+    filter: any = { };
+
     constructor(private vehicleService: VehicleService) { }
 
-    ngOnInit() { 
-        this.vehicleService.getVehicles()
-            .subscribe(vehicles => this.vehicles = vehicles);
+    ngOnInit() {
+        var sources = [
+            this.vehicleService.getMakes(),
+            this.vehicleService.getVehicles()
+        ];
+
+        Observable.forkJoin(sources).subscribe(data => {
+            this.makes = data[0];
+            this.vehicles = this.allVehicles = data[1];
+        });
+    }
+
+    onFilterChange() {
+        var vehicles = this.allVehicles;
+
+        if (this.filter.makeId) {
+            vehicles = vehicles.filter(v => v.make.id == this.filter.makeId);
+            var selectedMake = this.makes.find(m => m.id == this.filter.makeId);
+            this.models = selectedMake ? selectedMake.models : [];
+        }
+
+        if (this.filter.modelId) 
+            vehicles = vehicles.filter(v => v.model.id == this.filter.modelId);
+
+        if (this.filter.isRegistered)
+            vehicles = vehicles.filter(v => v.isRegistered == <any>this.filter.isRegistered)
+
+        this.vehicles = vehicles;
+    }
+
+    resetFilter() {
+        this.filter = {};
+        this.onFilterChange();
     }
 }
