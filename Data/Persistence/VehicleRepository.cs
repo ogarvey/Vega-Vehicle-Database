fcecb26629221ebeebb9ad754ce88dc79711d859
Oliver Garvey <ogarvey@gmail.com>
2017-09-22 13:36:25 +0100

    refactored sorting code

--- Data/Persistence/VehicleRepository.cs
+++ Data/Persistence/VehicleRepository.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Vega.Data.Models;
@@ -57,18 +59,19 @@ namespace Vega.Data.Persistence
             if (queryObj.IsRegistered.HasValue)
                 query = query.Where( v => v.IsRegistered == queryObj.IsRegistered.Value);
 
-            if(queryObj.SortBy == "make")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.Model.Make.Name) : query.OrderByDescending(v => v.Model.Make.Name);
+            var columnsMap = new Dictionary<string, Expression<Func<Vehicle, object>>>()
+            {
+                ["make"] = v => v.Model.Make.Name,
+                ["model"] = v => v.Model.Name,
+                ["contactName"] = v => v.ContactName,
+                ["id"] = v => v.Id
+            };
 
-            if(queryObj.SortBy == "model")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.Model.Name) : query.OrderByDescending(v => v.Model.Name);
-
-            if(queryObj.SortBy == "contactName")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.ContactName) : query.OrderByDescending(v => v.ContactName);
-
-            if(queryObj.SortBy == "id")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.Id) : query.OrderByDescending(v => v.Id);
-
+            if (queryObj.IsSortAscending)
+                query = query.OrderBy(columnsMap[queryObj.SortBy]);
+            else
+                query = query.OrderByDescending(columnsMap[queryObj.SortBy]);
+                
             return await query.ToListAsync();
         }
     }
